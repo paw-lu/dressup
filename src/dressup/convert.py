bc04c188e7d65c476faa2d040e841022586ada44
Paulo Costa <pcosta@firstrepublic.com>
2020-04-08 21:32:56 -0700

    Add optional character type paramater

--- src/dressup/convert.py
+++ src/dressup/convert.py
@@ -1,17 +1,42 @@
 """Convert unicode."""
-from typing import Dict
+from typing import Dict, Iterable, Optional, Union
 
 
-def convert_characters(characters: str) -> Dict[str, str]:
-    """Convert characters to different unicode types."""
+def convert_characters(
+    characters: str, char_types: Optional[Union[str, Iterable[str]]] = None
+) -> Dict[str, str]:
+    """Convert characters to different unicode types.
+
+    Args:
+        characters (str): The characters to convert.
+        char_types (str or iterable of strings): The type of unicode character
+            to convert to. Options are "circled" and "negative circled".
+            If left as None will return all types.
+
+    Returns:
+        A dictionary where the keys are unicode character types and the
+        values are the converted
+    """
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
     translator = {
-        "Circled": dict(zip("abcdefghijklmnopqrstuvwxyz", "ⓐⓑⓒⓓⓔⓕⓖⓗⓘⓙⓚⓛⓜⓝⓞⓟⓠⓡⓢⓣⓤⓥⓦⓧⓨⓩ"))
+        "Circled": dict(zip(alphabet, "ⓐⓑⓒⓓⓔⓕⓖⓗⓘⓙⓚⓛⓜⓝⓞⓟⓠⓡⓢⓣⓤⓥⓦⓧⓨⓩ")),
+        "Negative circled": dict(zip(alphabet, "🅐🅑🅒🅓🅔🅕🅖🅗🅘🅙🅚🅛🅜🅝🅞🅟🅠🅡🅢🅣🅤🅥🅦🅧🅨🅩")),
     }
+    if isinstance(char_types, str):
+        char_types = [char_types]
+    if char_types is not None:
+        char_types = [
+            f"{char_type[0].upper()}{char_type[1:].lower()}" for char_type in char_types
+        ]
+    else:
+        char_types = ["Circled", "Negative circled"]
+
     converted_characters = {
         character_type: "".join(
             translator[character_type].get(character, character)
             for character in characters
         )
-        for character_type in translator
+        for character_type in char_types
     }
+    # TODO: Add exception here for missing key
     return converted_characters
