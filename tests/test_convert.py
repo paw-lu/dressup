2ebf55df435965d00356a8df28ef177ffd0c6535
Paulo Costa <pcosta@firstrepublic.com>
2020-04-08 21:32:24 -0700

    Add paramaterized test for optional types

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -1,4 +1,8 @@
 """Test cases for the convert module."""
+from typing import Dict
+
+import pytest
+
 from dressup import convert
 
 
@@ -6,3 +10,14 @@ def test_converter() -> None:
     """It converts "hello" to ⓗⓔⓛⓛⓞ."""
     converted_characters = convert.convert_characters("hello")
     assert {"Circled": "ⓗⓔⓛⓛⓞ", "Negative circled": "🅗🅔🅛🅛🅞"} == converted_characters
+
+
+@pytest.mark.parametrize(
+    "char_types,expected_output", [("circled", {"Circled": "ⓗⓔⓛⓛⓞ"})]
+)
+def test_paramaterized_converter(
+    char_types: str, expected_output: Dict[str, str]
+) -> None:
+    """It converts "hello" to the specified unicode type."""
+    converted_characters = convert.convert_characters("hello", char_types=char_types)
+    assert expected_output == converted_characters
