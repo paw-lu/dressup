96c9325bd180c4004e638787614db23552af3b73
Paulo Costa <pcosta@firstrepublic.com>
2020-04-08 20:01:47 -0700

    Switch --version to callback

--- src/dressup/console.py
+++ src/dressup/console.py
@@ -6,9 +6,24 @@ from . import __version__
 app = typer.Typer()
 
 
+def version_callback(value: bool) -> None:
+    """Return the package version."""
+    if value:
+        typer.echo(f"Dress up version: {__version__}")
+        raise typer.Exit()
+    pass
+
+
 @app.command()
 def main(
-    version: bool = typer.Option(False, help="Return the package version.")
+    characters: str,
+    version: bool = typer.Option(
+        None,
+        "--version",
+        help="Return the package version." "",
+        callback=version_callback,
+        is_eager=True,
+    ),
 ) -> None:
     """Dress up your unicode!"""
     if version:
