dc21f43708fa971acf7acfc826dd809802a4e53c
Paulo Costa <PauloSCosta5@gmail.com>
2020-04-14 19:34:42 -0700

    Add message when no argument is provided

--- src/dressup/console.py
+++ src/dressup/console.py
@@ -17,7 +17,7 @@ def version_callback(value: bool) -> None:
 
 @app.command()
 def main(
-    characters: str,
+    characters: str = typer.Argument(None,),
     version: bool = typer.Option(
         None,
         "--version",
@@ -26,9 +26,18 @@ def main(
         is_eager=True,
     ),
 ) -> None:
-    """Dress up your unicode!"""
-    converted_characters = convert.convert_characters(characters)
-    for character_type, converted_character in converted_characters.items():
-        typer.secho(f"\n{character_type}", fg=typer.colors.MAGENTA, bold=True)
-        typer.secho(f"\n{converted_character}")
-    pass
+    """Dress up your unicode!
+
+    Args:
+        characters (str): The characters to convert.
+        version (bool): Whether to return the package version. Will
+            supersede all other arguments.
+    """
+    if characters is None:
+        typer.echo("No characters provided to convert.")
+    else:
+        converted_characters = convert.convert_characters(characters)
+        for character_type, converted_character in converted_characters.items():
+            typer.secho(f"\n{character_type}", fg=typer.colors.MAGENTA, bold=True)
+            typer.secho(f"\n{converted_character}")
+        pass
